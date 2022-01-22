33e33ff22f108288b7e70960e02f64f938578ea2
Bishoy <bishoy.samwuel@gmail.com>
2022-01-22 15:16:23 +0200

    Make reviewers required changes

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes,
-} from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import Message from './message';
 import configureStore from './configureStore';
+import { App } from './App';
 
 const store = configureStore();
 
-export default function App() {
+export default function Index() {
   return (
     <Provider store={store}>
-      <Router>
-        <div>
-          <App />
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/randomMessage" element={<Message msg="hi" />} />
-          </Routes>
-        </div>
-      </Router>
+      <App />
     </Provider>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
 ReactDOM.render(
-  <App />,
+  <Index />,
   document.getElementById('root'),
 );
--- src/message.jsx
+++ src/message.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const GET_RANDOM_MSG_REQUEST = 'GET_RANDOM_MSG_REQUEST';
 const GET_RANDOM_MSG_SUCCSESS = 'GET_RANDOM_MSG_SUCCSESS';
 
-const getRandomMsgSuccess = (json) => {
-  return {
-    type: GET_RANDOM_MSG_SUCCSESS,
-    json,
-  };
-};
+const getRandomMsgSuccess = (json) => ({
+  type: GET_RANDOM_MSG_SUCCSESS,
+  json,
+});
 
-//thunk
+// thunk
 const getRandomMsg = () => (dispacth) => {
   dispacth({ type: GET_RANDOM_MSG_REQUEST });
   return fetch('http://[::1]:4000/api/v1/randomMessage.json')
     .then((response) => response.json())
-    .then((json) => dispacth(getRandomMsgSuccess(json)))
-    .catch((error) => console.log(error));
+    .then((json) => dispacth(getRandomMsgSuccess(json)));
 };
 
-export default function Message(props) {
+const Message = (props) => {
   const { msg } = props;
   const dispatch = useDispatch();
   const message = useSelector((state) => state.msg);
   return (
     <div>
       <h2>
-        The Message from props is {msg}
+        The Message from props is
+        {' '}
+        {msg}
       </h2>
-      <button className="getMessageBtn" onClick={() => dispatch(getRandomMsg())}> Get Random Message</button>
+      <button type="button" className="getMessageBtn" onClick={() => dispatch(getRandomMsg())}> Get Random Message</button>
       <h2>
         The Message from store is
         {' '}
@@ -38,4 +38,10 @@ export default function Message(props) {
       </h2>
     </div>
   );
-}
+};
+
+Message.propTypes = {
+  msg: PropTypes.string.isRequired,
+};
+
+export default Message;
